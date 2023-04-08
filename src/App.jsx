b7f0dabb1d85e52a8e9bf3c817eff2e4eeac619f
Kostas Alex <sdi1700003@di.uoa.gr>
2023-04-08 14:04:51 +0300

    feat: Initialize project with basic setup
    To run the project locally, execute the following commands:
    $ npm install
    $ npm run dev

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-blue-500 font-bold border-2 cursor-pointer border-red-100 underline text-center shadow-md bg-yellow-50 hover:shadow-inner">ΤΕΔΙ Project</h1> 
+    </div>
+  )
+}
+
+export default App
