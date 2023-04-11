102b2bd75e09c445ce6e7d563b286dbeeaad4437
Kostas Alex <sdi1700003@di.uoa.gr>
2023-04-11 21:39:24 +0300

    feat(deployment):  enable successful deployment on Vercel
    + resolve configuration issues

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export {default as Header} from './common/header/Header';
 export {default as Login} from './auth/Login';
-export {default as SignUp} from './auth/SignUp'
+export {default as SignUp} from './auth/SignUp';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+  },
+}
