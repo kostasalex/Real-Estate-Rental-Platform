f2b7d2444d6dcddcc8b1afba794ba0500d6584cd
Babis St <mpamphsfe@gmail.com>
2023-04-14 17:04:55 +0300

    styling changes to cards

--- src/components/common/homepage/Card.jsx
+++ src/components/common/homepage/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Card = ({ title, image }) => (
+const Card = ({ title, image, price, dates }) => (
   <div className="square">
     <img src={image} alt={title} className="square__image" />
-    <h3 className="square__title">{title}</h3>
+    <h2 className="square__title">{title}</h2>
+    <p className="square__dates">{dates}</p>
+    <h1 className="square__price">$ {price} night</h1>
+    
   </div>
 );
 
--- src/components/common/homepage/Cards.jsx
+++ src/components/common/homepage/Cards.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import Card from "./Card";
 
 const data = [
-  { title: "Paris", image: "https://picsum.photos/id/237/200/200" },
-  { title: "Tokyo", image: "https://picsum.photos/id/238/200/200" },
-  { title: "New York", image: "https://picsum.photos/id/239/200/200" },
-  { title: "Sydney", image: "https://picsum.photos/id/240/200/200" },
-  { title: "London", image: "https://picsum.photos/id/241/200/200" },
-  { title: "Los Angeles", image: "https://picsum.photos/id/242/200/200" },
+  { title: "Paris", image: "https://picsum.photos/id/237/200/200", price: "100", dates: "Apr 10-23" },
+  { title: "Tokyo", image: "https://picsum.photos/id/238/200/200", price: "329", dates: "Apr 10-23" },
+  { title: "New York", image: "https://picsum.photos/id/239/200/200", price: "517", dates: "Apr 10-23" },
+  { title: "Sydney", image: "https://picsum.photos/id/240/200/200", price: "1,289", dates: "Apr 10-23" },
+  { title: "London", image: "https://picsum.photos/id/241/200/200", price: "399", dates: "Apr 10-23" },
+  { title: "Los Angeles", image: "https://picsum.photos/id/242/200/200", price: "263", dates: "Apr 10-23" },
 ];
 
 const Cards = () => (
   <div className="flex flex-wrap">
-    {data.map(({ title, image }, index) => (
-      <Card key={index} title={title} image={image} />
+    {data.map(({ title, image, price, dates }, index) => (
+      <Card key={index} title={title} image={image} price={price} dates={dates} />
     ))}
   </div>
 );
